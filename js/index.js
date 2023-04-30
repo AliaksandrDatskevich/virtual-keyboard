6f16966affe653381ec3dcc0ca8a944d8ddd6a57
Aliaksandr Datskevich <datskevich.a@icloud.com>
2023-04-30 21:38:56 +0300

    feat: add basic key layout

--- js/index.js
+++ js/index.js
@@ -1,22 +1,43 @@
+import keys from './keys';
+
 const outerWrapper = document.createElement('div');
 outerWrapper.className = 'wrapper';
+
 const title = document.createElement('p');
 title.textContent = 'Virtual Keyboard';
 title.className = 'title';
 outerWrapper.appendChild(title);
+
 const textArea = document.createElement('textarea');
 textArea.className = 'textarea';
 outerWrapper.appendChild(textArea);
+
 const keyboard = document.createElement('div');
 keyboard.className = 'keyboard';
-// TODO keys
+
+for (let i = 0; i < keys.length; i += 1) {
+  const row = document.createElement('div');
+  row.className = 'keyboard-row';
+  for (let j = 0; j < keys[i].length; j += 1) {
+    const key = document.createElement('div');
+    key.className = 'key';
+    const character = document.createElement('span');
+    character.textContent = keys[i][j].eng.caseUp;
+    key.appendChild(character);
+    row.appendChild(key);
+  }
+  keyboard.appendChild(row);
+}
 outerWrapper.appendChild(keyboard);
+
 const os = document.createElement('p');
 os.textContent = 'The keyboard was created in the Windows operating system';
 os.className = 'os';
 outerWrapper.appendChild(os);
+
 const lang = document.createElement('p');
 lang.textContent = 'To switch the language combination: left ctrl + alt';
 lang.className = 'lang';
 outerWrapper.appendChild(lang);
+
 document.body.appendChild(outerWrapper);
